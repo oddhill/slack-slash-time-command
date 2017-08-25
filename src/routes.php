de911c239c9ec91dc2ec36f2d0b659ddef30b790
Johan Falk <johan.falk@oddhill.se>
2017-08-25 14:05:48 +0200

    Changed route

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,9 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->post('/time', function ($request, $response, $args) {
+  // Sample log message
+  //$this->logger->info($request);
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+  return 'TIME!';
 });
