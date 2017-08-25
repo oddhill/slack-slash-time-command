fdfc445380f93eb6b77c7baaa57519775b56d8cc
Johan Falk <johan.falk@oddhill.se>
2017-08-25 14:18:57 +0200

    Added static json response

--- src/routes.php
+++ src/routes.php
@@ -2,8 +2,8 @@
 // Routes
 
 $app->post('/time', function ($request, $response, $args) {
-  // Sample log message
-  //$this->logger->info($request);
-
-  return 'TIME!';
+  return $response->withJson(array(
+    'response_type' => 'in_channel',
+    'text' => 'TIME!',
+  ));
 });
